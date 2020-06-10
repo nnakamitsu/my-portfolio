12274f3223740141ac43340ba490576ecce0859d
Nathan Nakamitsu <nathannakamitsu@gmail.com>
2020-06-10 18:05:47 +0000

    Calendar progress

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,55 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+  //   throw new UnsupportedOperationException("TODO: Implement this method.");
+    long duration = request.getDuration();
+    Collection<String> attendees =  request.getAttendees();
+    Collection<TimeRange> blockedtimes = new ArrayList<TimeRange>();
+    Collection<TimeRange> result = new ArrayList<TimeRange>();
+    for(Event event : events) {
+      boolean yes = false;
+      for(String attendee : event.getAttendees()) {
+        if (attendees.contains(attendee)) {
+          yes = true;
+          break;
+        }
+      }
+      if (yes) {
+        blockedtimes.add(event.getWhen());
+      }
+    }
+    Collections.sort(blockedtimes, TimeRange.ORDER_BY_START);
+    Collection<TimeRange> condensedblockedtimes = new ArrayList<TimeRange>();
+    for(int i = 0; i < blockedtimes.size() - 1; i++) {
+      TimeRange first = blockedtimes.get(i);
+      TimeRange next = blockedtimes.get(i+1);
+      if (first.contains(next)) {
+        blockedtimes.remove(next);
+        i--;
+      } else if (first.overlaps(next)) {
+        blockedtimes.add(i, new TimeRange(first.start(), next.end()));
+        i--;
+      } else {
+        condensedblockedtimes.add(i, blockedtimes.get(i));
+      }
+    }
+    for(int i = 0; i < condensedblockedtimes.size(); i++) {
+      if (i == 0) {
+        result.add(i, new TimeRange(TimeRange.START_OF_DAY, condensedblockedtimes.get(i).start()));
+      } else {
+        result.add(i, new TimeRange(condensedblockedtimes.get(i-1).end(), condensedblockedtimes.get(i).start()));
+      }
+    }
+    if (condensedblockedtimes.size() > 0) {
+      result.add(newTimeRange(condensedblockedtimes.get(condensedblockedtimes.size()-1), TimeRange.END_OF_DAY));
+    }
+    return result;
+
   }
 }
